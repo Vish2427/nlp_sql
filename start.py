1e427c2e13082fd6f8a27387c6661bfbd8cf5e51
Vishal Choudhary <98541876+Vish2427@users.noreply.github.com>
2024-10-29 19:26:54 +0530

    Update start.py

--- start.py
+++ start.py
@@ -1,9 +1,8 @@
-import streamlit as st
-from model_loading import MyVanna
-
 __import__('pysqlite3')
 import sys
 sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+import streamlit as st
+from model_loading import MyVanna
 
 vn=MyVanna(config={
     'n_results_sql':1,
